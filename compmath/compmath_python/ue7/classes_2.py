39c95186b4d0c7421db3aa9d5ee24420e05105c3
idahoenigmann <idahoney@gmx.at>
2021-05-07 17:28:48 +0200

    start with compmath ue7

---
+++ compmath/compmath_python/ue7/classes_2.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class Vector:
+    def add(self, z1, z2):
+        if len(z1) != len(z2):
+            raise ValueError("length of list must match")
+        return list(z1[i] + z2[i] for i in range(len(z1)))
+
+    def scalar(self, a, z1):
+        return list(a * e for e in z1)
+
+
+class VectorPlus(Vector):
+    def vector_proc(self, z1, z2):
+        return np.cross(np.array(z1), np.array(z2))
+
+    def tensor(self, z1, z2):
+        return np.tensordot(np.array(z1), np.array(z2))
+
+
+if __name__ == "__main__":
+    v = VectorPlus()
+
+    print(v.add([1, 2, 3], [1, 3, 5]))
+    print(v.scalar(42, [1, 3, 5]))
+    print(v.vector_proc([2, 4, 3], [3, -2, 6]))
+    print(v.tensor([1, 0, 0], [1, 0]))
