af519e950c9f2af5295376991c0104ffdea768af
idahoenigmann <idahoney@gmx.at>
2021-05-10 11:55:09 +0200

    work on compmath ue7

--- compmath/compmath_python/ue7/classes_2.py
+++ compmath/compmath_python/ue7/classes_2.py
@@ -13,10 +13,12 @@ class Vector:
 
 class VectorPlus(Vector):
     def vector_proc(self, z1, z2):
-        return np.cross(np.array(z1), np.array(z2))
+        return [z1[(i + 1) % len(z1)] * z2[(i + 2) % len(z2)] - z2[(i + 1) % len(z2)] * z1[(i + 2) % len(z1)] for i in
+                range(len(z1))]
+        # return np.cross(np.array(z1), np.array(z2))
 
     def tensor(self, z1, z2):
-        return np.tensordot(np.array(z1), np.array(z2))
+        return [[z1_el * z2_el for z2_el in z2] for z1_el in z1]
 
 
 if __name__ == "__main__":
@@ -24,5 +26,5 @@ if __name__ == "__main__":
 
     print(v.add([1, 2, 3], [1, 3, 5]))
     print(v.scalar(42, [1, 3, 5]))
-    print(v.vector_proc([2, 4, 3], [3, -2, 6]))
-    print(v.tensor([1, 0, 0], [1, 0]))
+    print(v.vector_proc([1, 2, 3], [3, 4, 5]))
+    print(v.tensor([1, 2, 3], [3, 4, 5]))
