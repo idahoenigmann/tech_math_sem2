c0c6f347a3fe402ed2de883bde1892c29c398338
idahoenigmann <idahoney@gmx.at>
2021-05-15 23:41:56 +0200

    finish (?) compmath ue8

--- compmath/compmath_python/ue8/1.py
+++ compmath/compmath_python/ue8/1.py
@@ -8,17 +8,6 @@ def dev(fun):
     return inner_dev
 
 
-def comp(l):
-    def inner_comp(val):
-        res = l[-1](val)
-        # print(l[-1])
-        for f in l[:-1][::-1]:
-            # print(f)
-            res = f(res)
-        return res
-    return inner_comp
-
-
 def count(f):
     counter = 0
 
@@ -61,9 +50,6 @@ class Test:
 if __name__ == "__main__":
     print(sin_(1))
     print(exp_(1))
-
-    sin_exp = comp([sin, exp, sin, sin])        # TODO
-    print(sin_exp(1))
 
     print(foo())
 
